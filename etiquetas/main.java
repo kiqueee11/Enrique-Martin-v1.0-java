6aa63668f6f6f980ffa041b1bd922a87e215d4da
Enrique <martinenrique838@gmail.com>
2024-10-17 10:44:01 +0200

    impresion de argumentos

--- etiquetas/main.java
+++ etiquetas/main.java
@@ -1,5 +1,9 @@
 public class Main{
 	public static void main(String[] args){
-		Sistem.out.println("");
+ 		if (args.lenght==0){
+			System.out.println("no hay argumentos");
+		}else{
+			System.out.println("hay "+ args.length() + "argumentos")
+		}
 	}
 }
